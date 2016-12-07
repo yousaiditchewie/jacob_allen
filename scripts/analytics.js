728c4e7e8a86337bd02486a4b932dcc5d0cc7dab
Jacob Allen <jedijake@me.com>
2016-12-06 16:58:51 -0800

    experiment further with Google Analytics

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -1,2 +1,25 @@
-// assign event listeners to all button interactions
-var projects = document.getElementById("#projectsLink");
+(function() {
+  // console.log("Analytics.js Loaded!");
+
+  // Save Google Analytics ID to memory
+  var gaID = "UA-85710672-1";
+
+
+  // assign event listeners to all button interactions
+
+  //INTRO SECTION
+  var $projects    = $("#projectsLink");
+  var $aboutMeLink = $("#aboutMe");
+  var $contactLink = $("#contactLink");
+  var $resumeLink  = $("#resumeLink");
+
+  // // write a function that sends tracking data to GA
+  // function projectsTracker() {
+  //   ga("projects.send", "pageview");
+  //   //console log for testing
+  //   console.log('projets sent');
+  // };
+
+  // call the function that sends tracking data to GA
+  $projects.click(projectsTracker);
+})();
